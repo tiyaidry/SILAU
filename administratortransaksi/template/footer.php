0c57f7e3d48c762c5db21d47525336c7138c317c
Sutarmo <sutarmo.sisfountan@gmail.com>
2017-07-12 13:06:00 +0700

    tiya

---
+++ administratortransaksi/template/footer.php
@@ -0,0 +1,23 @@
+		<div id="footer">
+				<span>
+					© 2017 Sistem Informasi Laundry by CISFO
+				</span>
+		</div>
+	</div>
+	</div>
+
+
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/metisMenu.min.js"></script>
+    <script>
+	    function hanyaAngka(evt) {
+		  var charCode = (evt.which) ? evt.which : event.keyCode
+		  if (charCode > 31 && (charCode < 13 || charCode > 57))
+		    	return false;
+			return true;
+		}
+	</script> 
+</body>
+</html>
---
+++ administratortransaksi/template/navigasi.php
@@ -0,0 +1,12 @@
+		<div class="navi" id="primary-navi-wrap">
+			<ul>
+		        <li class="home"><a href="homeA.html">Home</a></li>
+		        <li class="transakasi"><a href="transaksi.html">Transaksi</a></li>
+		        <li class="user"><a href="user.html">User</a></li>
+		        <li class="layanan"><a href="layanan.html">Layanan</a></li>
+		        <li class="pakaian"><a href="pakaian.html">Pakaian</a></li>
+		        <li class="keuangan"><a href="keuangan.html">Keuangan</a></li>
+		        <li class="gantirugi"><a href="gantirugi.html">Ganti Rugi</a></li>
+		        <li class="pelanggan"><a href="pelanggan.html">Pelanggan</a></li>
+			</ul>
+    	</div>
